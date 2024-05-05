9029d3b4f405c17af51bae187158db03c018a2af
pugMother <165683422+pugMother@users.noreply.github.com>
2024-05-05 12:23:46 +0300

    Update hw-8.js

--- hw-8.js
+++ hw-8.js
@@ -1,60 +1,60 @@
 //Task1
-//const people = [
-//    { name: 'Глеб', age: 29 },
-//    { name: 'Анна', age: 17 },
-//    { name: 'Олег', age: 7 },
-//    { name: 'Оксана', age: 47 }
-// ];
-// people.sort((person1, person2) => person1.age - person2.age);
-// console.log(people);
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ people.sort((person1, person2) => person1.age - person2.age);
+ console.log(people);
 
  //Task2
-// function isPositive(num) {
-//    return num > 0;
-// }
-// function isMale(person) {
-//    return person.gender === 'male';
-// }
-// function filter(arr, ruleFunction) {
-//    const result = [];
-//    for (let i = 0; i < arr.length; i++) {
-//        if (ruleFunction(arr[i])) {
-//            result.push(arr[i]);
-//        }
-//    }
-//    return result;
-// }
-// console.log(filter([3, -4, 1, 9], isPositive));
+ function isPositive(num) {
+    return num > 0;
+ }
+ function isMale(person) {
+    return person.gender === 'male';
+ }
+ function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+ }
+ console.log(filter([3, -4, 1, 9], isPositive));
 
-// const people = [
-//   {name: 'Глеб', gender: 'male'},
-//   {name: 'Анна', gender: 'female'},
-//   {name: 'Олег', gender: 'male'},
-//   {name: 'Оксана', gender: 'female'}
-// ];
+ const human = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+ ];
 
-// console.log(filter(people, isMale));
+ console.log(filter(human, isMale));
 
  //Task3
- //function logCurrentDate(){
- //   console.log(new Date());
- //}
- //const intervalId = setInterval(logCurrentDate, 3000);
- //setTimeout(() => {
- //   clearInterval(intervalId);
- //   console.log("30 секунд прошло");
- //}, 30000);
+ function logCurrentDate(){
+    console.log(new Date());
+ }
+ const intervalId = setInterval(logCurrentDate, 3000);
+ setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("30 секунд прошло");
+ }, 30000);
 
  //Task4
-// function delayForSecond(callback) {
-//    setTimeout(callback, 1000);
-// }
-// delayForSecond(function () {
-//   console.log('Привет, Глеб!');
-// })
+ function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+ }
+ delayForSecond(function () {
+   console.log('Привет, Глеб!');
+ })
 
 //Task5
-function delayForSecond(cb) {
+function delayForSecondTo(cb) {
     setTimeout(() => {
         console.log('Прошла одна секунда');
         if(cb) {  cb(); }
@@ -65,4 +65,4 @@ function sayHi (name) {
     console.log(`Привет, ${name}!`);
 }
 
-delayForSecond(() => sayHi('Глеб'));
+delayForSecondTo(() => sayHi('Глеб'));
